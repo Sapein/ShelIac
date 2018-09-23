42b0c0e1860b1c0d1604f965a5edd33d5c1d951a
Sapein <sapeint@hashbang.sh>
2018-09-23 19:02:46 -0400

    Designate Parser 'Phases' in comments

--- core/sheliac_core.sh
+++ core/sheliac_core.sh
@@ -58,34 +58,39 @@ SheliacCore_ScriptTranslate() {
     _temporary_file="${shs_parse_location}.${_script_name}.t"
 
     # Cache Check
-    if [ ! -f "${_shs_copy}" ]
+    if [ -f "${_shs_copy}" ]
     then
-        cp "${_script_path}"/"${_script_name}" "${_shs_copy}"
-    else
         _new_ver=$(cat "${_script_path}"/"${_script_name}")
         _old_ver=$(cat "${_shs_copy}")
         if [ "${_new_ver}" = "${_old_ver}" ] && [ "${_SheliacCore_CacheResults}" -eq 0 ]
-            return;
+            return 0;
         fi
+        rm "${_shs_copy}"
     fi
 
-    #Put in Hook Here - Pre-Processing Phase
+    #Pre-Processing Phase
+    cp "${_script_path}"/"${_script_name}" "${_shs_copy}"
 
+    #Variable Collecting Phase
     awk '$0 ~ /:=/ { FS=":="; print $1; }' "${_shs_copy}" > "${_var_file}"
     sed 's/ $//' "${_var_file}" | awk '!seen[$0]++' > "${_temporary_file}.vars"
     mv "${_temporary_file}.vars" "${_var_file}"
 
+    #Variable Whitespace Removal
     cp "${_shs_copy}" "${_sh_trans}"
     sed s/" :="/":="/ "${_shs_copy}" | sed s/":= "/":="/ > "${_sh_trans}"
 
+    #Function Return Change Phase
     awk '{if(index($0,":=")==0 || index($0, ")")==0) {print $0;}}
          $0 ~ /:=/ && $0 ~ /)/ { split($0,vars,":="); sub(" ", "", vars[1]); sub(")", " \"" vars[1] "\")");
          sub(vars[1], vars[1] "=0\n"); sub(":=", ""); print $0; }' "${_sh_trans}" | sed 's/^ //'  > "${_temporary_file}.sht"
     mv "${_temporary_file}.sht" "${_sh_trans}"
 
+    #POSIX Shell function translation
     sed 's/(/ /' "${_sh_trans}" | sed 's/)/ /' | sed s/" "$// > "${_temporary_file}.sht"
     mv "${_temporary_file}.sht" "${_sh_trans}"
 
+    #Variable Translation Part 1
     A_IFS="${IFS:-'N'}"
     unset IFS
     for var in $(cat "${_var_file}")
@@ -94,6 +99,7 @@ SheliacCore_ScriptTranslate() {
         mv "${_temporary_file}.sht" "${_sh_trans}"
     done
 
+    #Variable Translation Part 2 - Assignment References
     for var in $(cat "${_var_file}")
     do
         sed 's/:='"${var}"'[^A-Za-z0-9_]*/:="${'"${var}"'}"/' "${_sh_trans}" > "${_temporary_file}.sht"
@@ -101,10 +107,11 @@ SheliacCore_ScriptTranslate() {
     done
     if [ "${A_IFS}" != "N" ]; then IFS="${A_IFS}"; fi
 
+    #Translation to POSIX Shell Variable assignments
     sed 's/:=/=/g' "${_sh_trans}" > "${_temporary_file}.sht"
     mv "${_temporary_file}.sht" "${_sh_trans}"
 
-    # Do Built-in functions
+    #Translate Built-Ins to the written counterparts
     sed 's/install/SheliacCore_Install/g' "${_sh_trans}" > "${_temporary_file}.sht"
     mv "${_temporary_file}.sht" "${_sh_trans}"
 
@@ -117,7 +124,7 @@ SheliacCore_ScriptTranslate() {
     sed 's/server/SheliacCore_Server/g' "${_sh_trans}" > "${_temporary_file}.sht"
     mv "${_temporary_file}.sht" "${_sh_trans}"
 
-    #Finish Translation
+    #Turn to proper POSIX Shell file
     printf "#!/bin/sh\n" > "${_sh_output}"
     cat "${_sh_trans}" >> "${_sh_output}"
     chmod +x "${_sh_output}"
